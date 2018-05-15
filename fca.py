8b170d3e722c73104479d861e283a9b947a184c1
simplerick <32038973+simplerick@users.noreply.github.com>
2018-05-16 00:54:32 +0300

    Add files via upload

--- fca.py
+++ fca.py
@@ -9,13 +9,20 @@ class FCA:
     I - context, G - objects, M - attributes
     """
 
-    def __init__(self, data):
+    def __init__(self, data=np.zeros((0,0))):
         self.I = data.copy().astype(dtype='bool')
         self.G = np.arange(self.I.shape[0])
         self.M = np.arange(self.I.shape[1])
         self.lattice = {} 
         self.adj = {}   #adjacency lists for cover relation
+        self.reversed_adj = {}
         self.conf = {} 
+        self.support = {}
+        self.purity = {}
+        self.accuracy = {}
+        self.f_measure = {}
+        self.stability = {}
+        self.partition_to_classes = {}
 
 
 
@@ -83,12 +90,14 @@ class FCA:
 
 
 
-    def save_lattice(self, file_name='lattice'):
+    def save_lattice(self, path='lattice'):
         """
-        Save lattice to file with path specified by 'file_name'
-        Params: file_name = 'lattice'
+        Save lattice to file with specified path 
+        Params: path
         """
-        f = open(file_name, 'w')
+        f = open(path, 'w')
+        f.writelines(" ".join(map(str,self.G))+'\n')
+        f.writelines(" ".join(map(str,self.M))+'\n')
         for index, concept in self.lattice.items():
             f.writelines('%d;%s;%s\n' % (
                 index,
@@ -99,14 +108,16 @@ class FCA:
 
 
 
-    def load_lattice(self, file_name='lattice'):
+    def load_lattice(self, path='lattice'):
         """
-        Load lattice from file with path specified by 'file_name'
-        Params: file_name = 'lattice'
+        Load lattice from file with specified path
+        Params: path
         Returns: lattice
         """
         self.lattice = {}
-        f = open(file_name, 'r')
+        f = open(path, 'r')
+        self.G = np.array(f.readline().split())
+        self.M = np.array(f.readline().split())
         for line in f:
             row = line.strip('\n').split(';')
             self.lattice[int(row[0])] = {
@@ -118,67 +129,109 @@ class FCA:
 
 
 
+    def save_partition(self, path='partition'):
+        """
+        Save partition to file with specified path 
+        Params: path
+        """
+        np.save(path,self.partition_to_classes)
+
+
+
+    def load_partition(self, path='partition'):
+        """
+        Load partition from file with specified path 
+        Params: path
+        """
+        self.partition_to_classes = np.load(path+".npy").item()
+
+
+
     def calculate_conf(self):
         """
         Calculate confidence for every edge in diagram, returns and stores in 'conf', needs 'adj' with cover relation
         Return type: dict with pairs ("i_j":float), where "i_j" means edge 
-                     from concept i to concept j, i<j i.e. in the downward direction
+                     from concept i to concept j, intent(i)<intent(j) i.e. in the downward direction
         """
+        assert(len(self.adj)>0)
         self.conf = {}
-        for j in self.adj:
-            for i in self.adj[j]:
-                self.conf[str(i)+"_"+str(j)] = self.lattice[j]['extent']/self.lattice[i]['extent']
+        assert(len(self.adj)>0)
+        for i in self.adj:
+            for j in self.adj[i]:
+                self.conf[str(i)+"_"+str(j)] = len(self.lattice[j]['extent'])/len(self.lattice[i]['extent'])
         return self.conf
 
 
 
-    def select_concepts(self, classes, lower_supp = 0, upper_supp = 0,  purity_value = 0, accuracy_value = 0, f_measure_value = 0):
+    def calculate_properties(self, classes):
+        """
+        Calculate support, purity, accuracy and f-measure values
+        """
+        self.purity = {}
+        self.accuracy = {}
+        self.f_measure = {}
+        self.partition_to_classes = {}
+        assert(len(self.lattice)>0)
+        for index, concept in self.lattice.items():
+            self.support[index] = len(concept['extent'])/self.I.shape[0]
+            class_frequency = classes[concept['extent'], :].sum(axis=0)
+            top_class = np.argmax(class_frequency)
+            tp = class_frequency[top_class]
+            fp = len(concept['extent']) - tp
+            bool_inverse = np.ones(classes.shape[0], dtype='bool')
+            bool_inverse[concept['extent']] = False
+            fn = np.logical_and(bool_inverse,classes[:,top_class]).sum()
+            tn = classes.shape[0] - tp - fp - fn
+
+            self.purity[index] = class_frequency[top_class] / len(concept['extent'])
+            self.accuracy[index] = (tn+tp) / (tn+tp+fp+fn)
+            self.f_measure[index] = 2 * tp / (2*tp + fp + fn)
+            self.partition_to_classes[index] = top_class
+
+
+
+    def calculate_stability(self):
+        """
+        Calculate "delta" estimate of stability for each concept in lattice. 
+        Requires 'adj' with cover relation.
+        Return type: dict with pairs (int i : float j), where i - concept index, j - stability 
+        """
+        assert(len(self.adj)>0)
+        self.stability = {}
+        for i in self.adj:
+            self.stability[i] = 0
+            for j in self.adj[i]:
+                self.stability[i] -= 0.5**(len(self.lattice[i]['extent'])-len(self.lattice[j]['extent']))
+        return self.stability
+
+
+
+    def select_concepts(self, lower_supp = 0, upper_supp = 0,  purity_value = 0, accuracy_value = 0, f_measure_value = 0, stability_value = 0):
         """
         Select concepts satisfying at least one of the conditions:
         1) support of concept > upper_supp
         2) lower_supp <= support <= upper_supp and purity, f-measure and accuracy >= than the values in parameters
-        Moreover for second case return new concept index (after reducing lattice to selected concepts) 
-        and its top class - the most common class in the extent.
         Params: upper_supp = 0, lower_supp = 0, purity_value = 0, accuracy_value = 0, f_measure_value = 0
                 classes - binary np.array where each column corresponds to class.
-        Returns: list of original indices of selected concepts, dict with pairs (int class, list of  new concept indices)
+        Returns: list of indices of selected concepts
         """
         concepts = []
-        top_classes = dict((c,[]) for c in range(classes.shape[1]))
-        new_index = 0
-
+        
         if (upper_supp == 0) or (purity_value == 0 and accuracy_value == 0 and f_measure_value == 0):
-            for index, concept in self.lattice.items():
-                if len(concept['extent'])/self.I.shape[0] >= lower_supp:
+            for index in self.lattice.items():
+                if self.support[index] >= lower_supp:
                     concepts.append(index)
             return concepts, top_classes
 
         for index, concept in self.lattice.items():
-            supp = len(concept['extent'])/self.I.shape[0]
-            if supp > upper_supp:
+            if self.support[index] > upper_supp:
                 concepts.append(index)
-                new_index += 1
                 continue
-            if supp >= lower_supp:
-                class_frequency = classes[concept['extent'], :].sum(axis=0)
-                top_class = np.argmax(class_frequency)
-                tp = class_frequency[top_class]
-                fp = len(concept['extent']) - tp
-                bool_inverse = np.ones(classes.shape[0], dtype='bool')
-                bool_inverse[concept['extent']] = False
-                fn = np.logical_and(bool_inverse,classes[:,top_class]).sum()
-                tn = classes.shape[0] - tp - fp - fn
-
-                purity= class_frequency[top_class] / len(concept['extent'])
-                accuracy = (tn+tp) / (tn+tp+fp+fn)
-                f_measure = 2 * tp / (2*tp + fp + fn)
-
-                if (purity >= purity_value and accuracy >= accuracy_value and f_measure >= f_measure_value):
+            if self.support[index] >= lower_supp:
+                if (self.purity[index] >= purity_value and self.accuracy[index] >= accuracy_value and self.f_measure[index] >= f_measure_value and self.stability[index] >= stability_value):
                     concepts.append(index)
-                    top_classes[top_class].append(new_index)
-                    new_index += 1
 
-        return concepts, top_classes
+        return concepts
 
 
 
@@ -189,20 +242,23 @@ class FCA:
         Returns: reduced lattice
         """
         lattice = {}
+        partition = {}
         new_index = 0
         for index, concept in self.lattice.items():
             if index in concept_indices:
                 lattice[new_index] = self.lattice[index]
+                partition[new_index] = self.partition_to_classes[index]
                 new_index += 1
         self.lattice = lattice
+        self.partition_to_classes = partition
         return self.lattice
 
 
 
-    def build_cover_relation(self):
+    def build_cover_relation(self, reverse=False):
         """
-        Calculate cover relation given lattice
-        Returns: adjacency list
+        Calculate cover relation given lattice, i.e. iRj iff intent(i)<intent(j)
+        Returns: adjacency list or adjacency list of reversed relation if reverse = True
         Return type: dict with pairs (int:set)
         """
         n = len(self.lattice)
@@ -221,10 +277,12 @@ class FCA:
                             break
                     if neighbor:
                         self.adj[i].add(j)
+        
+        self.reversed_adj = dict((i,set()) for i in range(n))
+        for i in self.adj:
+            for j in self.adj[i]:
+                self.reversed_adj[j].add(i)
+        if reverse:
+            return self.reversed_adj
 
         return self.adj
-
-
-
-
-
--- neural_network.py
+++ neural_network.py
@@ -1,144 +1,171 @@
-import numpy as np
 import tensorflow as tf
-from FCA import*
-from data_loader import *
+import numpy as np
 import time
 
 
-X, y, object_labels, attribute_labels = get_titanic()[:4]
-y_cl = one_hot(y, n_classes=2)
-X_train, y_train, X_val, y_val, X_test, y_test = train_test_split(
-    X, y_cl, tp=0.6, vp=0.2)
+
+def model(adj,res_connect, X_train, y_train, X_test, y_test, prob = {}, optimizer="gradient_descent",learning_rate=0.5, tests=3, num_epoch= 1000, config=""):
+
+  dim_in = X_train.shape[1]
+  dim_out = y_train.shape[1]
+  batch_size = 100
+  n_samples = X_train.shape[0]
+  first_level = []
+  last_level = {}
+  weight = {}
+  bias = {}
+
+  # n = np.array([tf.Variable(0.) for _ in range(len(adj))])
+  n = np.array([tf.Variable(0., name="Node") for _ in range(len(adj))])
+
+  tf.reset_default_graph()
+  sess = tf.Session()
+
+  x = tf.placeholder(tf.float32, [None, dim_in], name="x")
+  y_ = tf.placeholder(tf.float32, [None, dim_out], name="labels")
 
 
-dim_in = X_train.shape[1]
-dim_out = y_train.shape[1]
+  # initialization
+  print('Initialization')
+
+  last_level = set(adj.keys())
+
+  with tf.name_scope("lattice"):
+      for i in adj:
+          if len(adj[i]) == 0:
+              first_level.append(i)
+          for j in adj[i]:
+              last_level.discard(j)
+              weight[str(i) + ',' + str(j)] = tf.Variable(tf.truncated_normal([1, 1], stddev=0.1), name="W_lattice")
+              tf.summary.histogram("weights", weight[str(i) + ',' + str(j)])
+          bias[str(i)] = tf.Variable(tf.truncated_normal([1, 1], stddev=0.1), name="B_lattice")
+          tf.summary.histogram("biases", bias[str(i)])
 
 
-fca = FCA(X_train)
-fca.build_lattice()
-good_concepts, res_connect = fca.select_concepts(y_cl, lower_supp=0, upper_supp = 0.4,  purity_value = 0.75, accuracy_value = 0.5, f_measure_value=0.2)
-fca.reduce_lattice(good_concepts)
-adj = fca.build_cover_relation()
+  last_level = list(last_level)
+  print(first_level)
+  print(last_level)
 
-print("Number of concepts: " + str(len(fca.lattice)))
-print(res_connect)
+  # weight['in'] = tf.Variable(tf.truncated_normal([dim_in,len(first_level)], stddev = 0.1))
+  # bias['in'] = tf.Variable(tf.truncated_normal([len(first_level)], stddev = 0.1))
 
-first_level = []
-last_level = {}
-weight = {}
-bias = {}
-# n = np.array([tf.Variable(0.) for _ in range(len(adj))])
-n = np.array([tf.Variable(0.) for _ in range(len(adj))])
+  # weight['out'] = tf.Variable(tf.truncated_normal([len(last_level), dim_out], stddev = 0.1))
+  # bias['out'] = tf.Variable(tf.truncated_normal([dim_out], stddev = 0.1))
 
-x = tf.placeholder(tf.float32, [None, dim_in])
-y_ = tf.placeholder(tf.float32, [None, dim_out])
+  for i in first_level:
+      weight['in,'+str(i)] = tf.Variable(tf.truncated_normal([dim_in, 1], stddev=0.1), name="W_in")
+      tf.summary.histogram("weights", weight['in,'+str(i)])
 
+  for i in last_level:
+      weight[str(i)+',out'] = tf.Variable(tf.truncated_normal([1, dim_out], stddev=0.1), name="W_out")
+      tf.summary.histogram("weights", weight[str(i)+',out'])
 
-# initialization
-print('Initialization')
+  bias['out'] = tf.Variable(tf.truncated_normal([1,dim_out], stddev=0.1), name="B_out")
+  tf.summary.histogram("biases", bias['out'])
 
-last_level = set(adj.keys())
-
-for i in adj:
-    if len(adj[i]) == 0:
-        first_level.append(i)
-    for j in adj[i]:
-        last_level.discard(j)
-        weight[str(i) + ',' + str(j)
-               ] = tf.Variable(tf.truncated_normal([1, 1], stddev=0.1))
-    bias[str(i)] = tf.Variable(tf.truncated_normal([1, 1], stddev=0.1))
-
-last_level = list(last_level)
-
-# weight['in'] = tf.Variable(tf.truncated_normal([dim_in,len(first_level)], stddev = 0.1))
-# bias['in'] = tf.Variable(tf.truncated_normal([len(first_level)], stddev = 0.1))
-
-# weight['out'] = tf.Variable(tf.truncated_normal([len(last_level), dim_out], stddev = 0.1))
-# bias['out'] = tf.Variable(tf.truncated_normal([dim_out], stddev = 0.1))
-
-for i in first_level:
-    weight['in,'+str(i)] = tf.Variable(tf.truncated_normal([dim_in, 1], stddev=0.1))
-
-for i in last_level:
-    weight[str(i)+',out'] = tf.Variable(tf.truncated_normal([1, dim_out], stddev=0.1))
-
-for c in res_connect:
-    temp = np.zeros(dim_out)
-    temp[c] = 1
-    weight['res_'+str(c)] = tf.constant(temp, dtype=tf.float32)
-
-
-bias['out'] = tf.Variable(tf.truncated_normal([1,dim_out], stddev=0.1))
-
-
-# network connecting
-print('Network connecting')
-
-for i in first_level:
-    n[i] = tf.nn.relu(tf.matmul(x, weight['in,'+str(i)]) + bias[str(i)])
-
-def process(v):
-    activation = tf.Variable(0.)
-    if len(adj[v]) == 0:
-        return
-    for w in adj[v]:
-        process(w)
-        activation += weight[str(v) + ',' + str(w)] * n[w]
-    n[v] = tf.nn.relu(activation + bias[str(v)])
-
-for v in last_level:
-    process(v)
-
-y0 = bias['out']
-for i in last_level:
-    y0 += n[i] * weight[str(i)+',out']
-
-
-for c in res_connect:
-    activation = tf.Variable(0.)
-    for i in res_connect[c]:
-        activation += n[i]
-    y0 += activation * weight['res_'+str(c)]
+  with tf.name_scope("resnet"):
+      for c in res_connect:
+          temp = np.zeros(dim_out)
+          temp[c] = 1
+          weight['res_'+str(c)] = tf.constant(temp, dtype=tf.float32, name="W_res")
+          tf.summary.histogram("weights",  weight['res_'+str(c)])
 
 
 
-y = tf.nn.softmax(y0)
+  # network connecting
+  print('Network connecting')
+
+  for i in first_level:
+      n[i] = tf.nn.relu(tf.matmul(x, weight['in,'+str(i)]) + bias[str(i)])
+
+  def process(v):
+      activation = tf.Variable(0.)
+      if len(adj[v]) == 0:
+          return
+      for w in adj[v]:
+          process(w)
+          activation += weight[str(v) + ',' + str(w)] * n[w]
+          # activation += tf.nn.dropout(weight[str(v) + ',' + str(w)],keep_prob=prob[str(w)+"_"+str(v)]) * n[w]
+      n[v] = tf.nn.relu(activation + bias[str(v)])
+
+  for v in last_level:
+      process(v)
+
+  y0 = bias['out']
+  for i in last_level:
+      y0 += n[i] * weight[str(i)+',out']
 
 
-# training
-print('Training')
+  for c in res_connect:
+      activation = tf.Variable(0.)
+      for i in res_connect[c]:
+          activation += n[i]
+      y0 += activation * weight['res_'+str(c)]
 
 
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ *
-                                              tf.log(y), reduction_indices=[1]))
-train_step = tf.train.MomentumOptimizer(0.003,0.85).minimize(cross_entropy)
+  y = tf.nn.softmax(y0)
 
 
-tests = 5
-results = np.zeros((tests,))
-times = np.zeros((tests,))  #times in ms
-for test in range(tests):
-    tic = time.time()
-    init = tf.global_variables_initializer()
-    sess = tf.Session()
-    sess.run(init)
-    for i in range(1000):
-        sess.run(train_step, feed_dict={x: X_train, y_: y_train})
+  # training
+  print('Training')
 
+  with tf.name_scope("cross_entropy"):
+      cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ *
+                                                tf.log(y), reduction_indices=[1]))
+      tf.summary.scalar("cross_entropy", cross_entropy)
+  # train_step = tf.train.MomentumOptimizer(0.003,0.85).minimize(cross_entropy)
+  with tf.name_scope("train"):
+    if optimizer=="gradient_descent":
+      train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+    elif optimizer == "adagrad":
+      train_step = tf.train.AdagradOptimizer(learning_rate).minimize(cross_entropy)
+    elif optimizer == "adam":
+      train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+
+  with tf.name_scope("accuracy"):
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.summary.scalar("accuracy", accuracy)
 
-    toc = time.time()
+  
+  summ = tf.summary.merge_all()
+      
 
-    results[test] = sess.run(accuracy, feed_dict={x: X_test, y_: y_test})
-    times[test] = 1000*(toc - tic)
+
+  results = np.zeros((tests,))
+  times = np.zeros((tests,))  #times in ms
+  for test in range(tests):
+      writer = tf.summary.FileWriter("tb_data/"+config+str(test))
+      tic = time.time()
+      init = tf.global_variables_initializer()
+      sess = tf.Session()
+      sess.run(init)
+      writer.add_graph(sess.graph)
+      for i in range(num_epoch):
+          indices = np.random.choice(n_samples, batch_size)
+          x_batch, y_batch = X_train[indices], y_train[indices]
+          if i % 5 == 0:
+                [train_accuracy,s] = sess.run([accuracy,summ], feed_dict={x: x_batch, y_: y_batch})
+                writer.add_summary(s, i)
+          sess.run(train_step, feed_dict={x: x_batch, y_: y_batch})
+
+      toc = time.time()
+
+      results[test] = sess.run(accuracy, feed_dict={x: X_test, y_: y_test})
+      times[test] = 1000*(toc - tic)
 
 
-print('\n')
-for i in range(tests):
-    print("Test " + str(i+1) + " :  Accuracy = " + str(results[i]) + ", Time = " + str(times[i]))
+  # print('\n')
+  # for i in range(tests):
+  #     print("Test " + str(i+1) + " :  Accuracy = " + str(results[i]) + ", Time = " + str(times[i]))
+  # print("\nMean value of accuracy: " + str(results.mean()))
+  # print("Mean value of time: " + str(times.mean())+"\n")
+  print(config)
+  return results, times
 
-print("\nMean value of accuracy: " + str(results.mean()))
+
+
+
+
+
